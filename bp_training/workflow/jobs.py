c851ab858ad228494efa02816aa490fb961b2596
ben-pollard <benpollard0@googlemail.com>
2024-11-20 21:34:09 +0000

    refactor: simplify training job by removing unnecessary resume logic and configuration setup

--- bp_training/workflow/jobs.py
+++ bp_training/workflow/jobs.py
@@ -3,29 +3,11 @@ from bp_training.workflow import ops
 
 # from bp_training.workflow import assets
 # from bp_training.workflow import definitions
-from bp_training.trainer import TrainingConfig
 
-
-class JobConfig(Config):
-    resume: bool
-
-
-@op
-def provide_resume() -> bool:
-    return False
 
 @job
 def training_job():
-    job_config = JobConfig(resume=False)
-    training_config = TrainingConfig(
-        experiment_name="NER Test",
-        run_id="test_run_id",
-        run_name="test_run",
-        artifact_location="mlartifacts",
-        max_epochs=2,
-    )
     data_module = ops.load_data_op()
-    resume = provide_resume()
-    model = ops.get_model(resume=resume, data_module=data_module)
-    trained_model = ops.train_model(model, data_module, training_config)
+    model = ops.get_model_op(data_module=data_module)
+    trained_model = ops.train_model_op(model, data_module)
     # log_model_asset(trained_model)
