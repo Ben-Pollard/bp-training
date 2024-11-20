f88602873d869249fe9c13ea0bd94e0c5e3d5554
ben-pollard <benpollard0@googlemail.com>
2024-11-20 21:54:37 +0000

    feat: add configuration classes for data loading and model training in workflow config

--- bp_training/workflow/config.py
+++ bp_training/workflow/config.py
@@ -1,15 +1,27 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import MLFlowLogger
 from bp_training.util.tracking import MLFlowNoSaveModelCheckpoint
+from dagster import Config
 
-class TrainingConfig:
+
+class LoadDataConfig(Config):
+    # Define any configuration parameters needed for loading data
+    pass
+
+
+class GetModelConfig(Config):
+    resume: bool
+
+
+class TrainModelConfig(Config):
     experiment_name: str
     run_id: str
     run_name: str
     artifact_location: str
     max_epochs: int
 
-def configure_trainer(config: TrainingConfig) -> Trainer:
+
+def configure_trainer(config: TrainModelConfig) -> Trainer:
     mlflow_logger = MLFlowLogger(
         experiment_name=config.experiment_name,
         run_id=config.run_id,
