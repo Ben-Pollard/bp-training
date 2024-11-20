a6daa8a05a57df89488b20c55461b39f104556cc
ben-pollard <benpollard0@googlemail.com>
2024-11-20 21:44:26 +0000

    feat: enhance training job test with configuration and instance setup

--- tests/test_training_job.py
+++ tests/test_training_job.py
@@ -1,9 +1,22 @@
-from dagster import execute_job
-from bp_training.workflow.jobs import training_job
+from dagster import execute_job, DagsterInstance, RunConfig
+from bp_training.workflow.jobs import training_job, JobConfig
+from bp_training.workflow.ops import TrainingConfig
+
 
 def test_training_job():
-    result = execute_job(
-        training_job,
-        run_config={}
+    config = {}
+    job_config = JobConfig(resume=False)
+
+    training_config = TrainingConfig(
+        experiment_name="NER Test",
+        run_id="test_run_id",
+        run_name="test_run",
+        artifact_location="mlartifacts",
+        max_epochs=2,
     )
+
+    run_config = RunConfig({"get_model": job_config, "train_model": training_config})
+
+    instance = DagsterInstance.ephemeral()
+    result = training_job.execute_in_process(run_config=config, instance=instance)
     assert result.success
